7a41ce825ab36a3e1d0d414ec59ebc2504870b3a
shuizhengqi1 <shuizhengqi1@163.com>
2017-03-03 11:33:34 +0800

    进程管理

---
+++ ModernUIApp1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+namespace ModernUIApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetProcess();
+        }
+
+        public void GetProcess()
+        {
+            Process[] process = Process.GetProcesses();
+            for (int i = 0; i < process.Length; i++)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.MouseDoubleClick += lvi_MouseDoubleClick;
+                ContextMenu menu = new System.Windows.Controls.ContextMenu();
+                MenuItem stop = new MenuItem();
+                stop.Click += stop_Click;
+                stop.Header = "停止该进程";
+                menu.Items.Add(stop);
+                lvi.ContextMenu = menu;
+                lvi.Content = process[i].ProcessName;
+                List.Items.Add(lvi);
+            }
+        }
+
+        void stop_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            
+
+        }
+
+        void lvi_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListViewItem btn = sender as ListViewItem;
+            String content = btn.Content.ToString();
+            MessageBox.Show(content);
+            
+            ;
+        }
+    }
+}
